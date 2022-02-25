1650466aae45272675ed5d38c64587be0f4cb1f0
William <mrsandywilly@protonmail.com>
2022-02-25 10:57:50 +0000

    migrated to vite

---
+++ loader.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+    const loader = document.getElementById('loader');
+    const fadeEffect = setInterval(() => {
+        if (!loader.style.opacity) {
+          loader.style.opacity = 1;
+        }
+        if (loader.style.opacity > 0) {
+          loader.style.opacity -= 0.1;
+        } else {
+          loader.style.display = 'none';
+          clearInterval(fadeEffect);
+        }
+    }, 25);
+  });
